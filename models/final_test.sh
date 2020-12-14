661b3501d5f2fba465efa3a0f3745a7d8f4732a4
Huy Vinh Le <huyvinhle@msn.com>
2020-12-14 06:12:42 +0000

    corrected paths

--- models/final_test.sh
+++ models/final_test.sh
@@ -1,2 +1,2 @@
 rosservice call gazebo/delete_model final_setup_test
-rosrun gazebo_ros spawn_model -file final_test.sdf -sdf -model final_setup_test -y -0.0 -x -0.0 -z 0.0
+rosrun gazebo_ros spawn_model -file ~/etc/LeHuyVinh/models/final_test.sdf -sdf -model final_setup_test -y -0.0 -x -0.0 -z 0.0
